c58a8bb4248717e2a4cb486a2471e095768060d7
jackcase04 <jackcase04@gmail.com>
2025-01-28 18:37:16 -0600

    removed unused options component

--- components/index.js
+++ components/index.js
@@ -3,7 +3,6 @@ import Login from "./home/login/Login";
 import Welcome from "./home/welcome/Welcome";
 import Chores from "./home/chores/Chores";
 import Header from "./home/header/Header";
-import Options from "./home/header/options/options";
 import ReminderPicker from "./home/chores/reminderPicker/ReminderPicker";
 
 export {
@@ -11,6 +10,5 @@ export {
     Welcome,
     Chores,
     Header,
-    Options,
     ReminderPicker
 };
