3c3d0f5957168ec1fbb3cd2add93fe28b4da8eed
jackcase04 <jackcase04@gmail.com>
2025-07-24 12:36:02 -0500

    switched to app.config.js instead of app.json

--- app.config.js
+++ app.config.js
@@ -1,5 +1,48 @@
 export default {
-  android: {
-    googleServicesFile: process.env.GOOGLE_SERVICES_JSON ?? './google-services.json',
-  },
+  expo: {
+    scheme: "acme",
+    plugins: [
+      "expo-router"
+    ],
+    assetBundlePatterns: [
+      "**/*"
+    ],
+    name: "CCH App",
+    slug: "cch-house-app",
+    orientation: "portrait",
+    extra: {
+      router: {
+        origin: false
+      },
+      eas: {
+        projectId: "b3adeaa2-9352-48d6-9170-d82d93300aed"
+      }
+    },
+    version: "1.0.2",
+    android: {
+      package: "com.jackcase04.cchhouseapp",
+      useNextNotificationsApi: true,
+      googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
+      permissions: [
+        "SCHEDULE_EXACT_ALARM",
+        "USE_EXACT_ALARM"
+      ]
+    },
+    splash: {
+      image: "./assets/splashscreen_logo.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
+    },
+    ios: {
+      bundleIdentifier: "com.jackcase04.cchhouseapp",
+      infoPlist: {
+        UIBackgroundModes: [
+          "remote-notification",
+          "remote-notification"
+        ],
+        ITSAppUsesNonExemptEncryption: false
+      }
+    },
+    icon: "./assets/appstore.png"
+  }
 };
