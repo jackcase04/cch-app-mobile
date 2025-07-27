99ee783dff25c6f26dcf677c2c90d9a9593be745
jackcase04 <jackcase04@gmail.com>
2025-07-27 18:50:31 -0500

    reverted app.config.js

--- app.config.js
+++ app.config.js
@@ -1,5 +1,6 @@
 export default {
   expo: {
+    scheme: "acme",
     plugins: [
       "expo-router"
     ],
@@ -10,27 +11,38 @@ export default {
     slug: "cch-house-app",
     orientation: "portrait",
     extra: {
+      router: {
+        origin: false
+      },
       eas: {
         projectId: "b3adeaa2-9352-48d6-9170-d82d93300aed"
       }
     },
-    version: "1.0.2",
+    version: "2.0.1",
     android: {
       package: "com.jackcase04.cchhouseapp",
       useNextNotificationsApi: true,
-      googleServicesFile: process.env.GOOGLE_SERVICES_JSON
+      googleServicesFile: process.env.GOOGLE_SERVICES_JSON,
+      permissions: [
+        "SCHEDULE_EXACT_ALARM",
+        "USE_EXACT_ALARM"
+      ]
+    },
+    splash: {
+      image: "./assets/splashscreen_logo.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff"
     },
     ios: {
       bundleIdentifier: "com.jackcase04.cchhouseapp",
       infoPlist: {
-        UIBackgroundModes: ["remote-notification"],
-        ITSAppUsesNonExemptEncryption: false,
-        NSAppTransportSecurity: {
-          NSAllowsArbitraryLoads: true
-        }
+        UIBackgroundModes: [
+          "remote-notification"
+        ],
+        ITSAppUsesNonExemptEncryption: false
       },
       entitlements: {
-        "aps-environment": process.env.EAS_BUILD_PROFILE === "production" ? "production" : "development"
+        "aps-environment": "development"
       }
     },
     icon: "./assets/appstore.png"
