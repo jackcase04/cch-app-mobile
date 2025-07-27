ccaaef56701bbfe241fc5b7296cddf4bb5d059fc
jackcase04 <jackcase04@gmail.com>
2025-07-26 19:46:55 -0500

    fixed malformed api call

--- services/healthService.js
+++ services/healthService.js
@@ -5,7 +5,7 @@ const api_url = API_URL;
 
 export const getHealth = async () => {
     try {
-        const response = await axios.get(`${api_url}/health`);
+        const response = await axios.get(`${api_url}/health/`);
 
         return {
             success: true
